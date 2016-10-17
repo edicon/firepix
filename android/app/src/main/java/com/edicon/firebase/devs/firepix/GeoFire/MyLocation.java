5fe34f390f16398d4572b075bb6a935144ec1c16
EDICON <hslee.edicon@gmail.com>
2016-10-18 00:47:18 +0900

    -Debugging.. GeoFire: OK

--- android/app/src/main/java/com/edicon/firebase/devs/firepix/GeoFire/MyLocation.java
+++ android/app/src/main/java/com/edicon/firebase/devs/firepix/GeoFire/MyLocation.java
@@ -114,18 +114,18 @@ public class MyLocation  {
 
     // ToDo: Check Ref. Source
     private void showMap(double latitude, double longitude) {
-        if( MyGeoFireNew.getMap() != null )
-            MyGeoFireNew.showMap( latitude, longitude);
+        if( MyGeoFire.getMap() != null )
+            MyGeoFire.showMap( latitude, longitude);
     }
 
     private void sendLocationToGeoFire(String userKey, double latitude, double longitude) {
-        if( MyGeoFireNew.getGeoFire() != null )
-            MyGeoFireNew.sendLocationToGeoFire( userKey, latitude, longitude);
+        if( MyGeoFire.getGeoFire() != null )
+            MyGeoFire.sendLocationToGeoFire( userKey, latitude, longitude);
     }
 
-    public void onStart( GoogleApiClient apiClient, MyGeoFireNew myGeoFire ) {
+    public void onStart( GoogleApiClient apiClient, MyGeoFire myGeoFire ) {
         // this.geoQuery.addGeoQueryEventListener(this);
-        MyGeoFireNew.addGeoQueryEventListener( myGeoFire );
+        MyGeoFire.addGeoQueryEventListener( myGeoFire );
         apiClient.connect();
     }
 }
